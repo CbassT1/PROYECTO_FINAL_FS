ffa4c7ceeda28812dd8be80ddcd2be4412fb8bb5
al07038321@tecmilenio.mx <Sdmt040801@>
2026-03-06 09:05:22 -0600

    fix: limpieza de marcadores residuales de git en authMiddleware

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -8,17 +8,12 @@ exports.verificarToken = (req, res, next) => {
     if (!token) return res.status(401).json({ mensaje: 'Formato de token inválido' });
 
     try {
-<<<<<<< HEAD
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
         // Identificamos qué tipo de sesión es
         if (decoded.user) req.user = decoded.user;                 // Token de Empleado/Admin
         if (decoded.empresaAuth) req.empresaAuth = decoded.empresaAuth; // Token temporal de Empresa
 
-=======
-        const cifrado = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = cifrado.user;
->>>>>>> 5f91839973e0139193a4a8306167e39f341d9ce5
         next();
     } catch (error) {
         res.status(401).json({ mensaje: 'Token no válido o expirado' });
